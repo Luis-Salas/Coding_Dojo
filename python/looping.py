3ece2a85051f9775a688a811cd0afddae0fbda08
= <salas.luis40@gmail.com>
2016-09-06 09:15:20 -0500

    adding python folder

---
+++ python/looping.py
@@ -0,0 +1,10 @@
+#printing odd numbers
+for i in range(1,1001):
+        if i%2 !=0:
+            print i
+
+
+#printing multiples of 5
+for i in range(5, 1000001):
+        if(i % 5 == 0):
+            print i
